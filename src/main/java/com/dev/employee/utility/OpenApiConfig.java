35aa0f4c549861ee52915cc376fa3c9212f8123e
aprilnurf <aprilia.fauziyah@tiket.com>
2023-05-24 21:20:29 +0700

    update properties

--- src/main/java/com/dev/employee/utility/OpenApiConfig.java
+++ src/main/java/com/dev/employee/utility/OpenApiConfig.java
@@ -11,8 +11,8 @@ public class OpenApiConfig {
   @Bean
   public OpenAPI usersMicroserviceOpenAPI() {
     return new OpenAPI()
-        .info(new Info().title("Your API Title")
-            .description("Your API Description")
+        .info(new Info().title("Employee App")
+            .description("Employee API")
             .version("1.0"));
   }
 
